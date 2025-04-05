57cad662e45b892428120480f4a1fe6dc5446a28
isHardCoded <ishardcoded@gmail.com>
2025-04-05 11:16:33 +0300

    Реализовали карточку товара

---
+++ src/components/Product.jsx
@@ -0,0 +1,22 @@
+const Product = () => {
+	return (
+		<div
+			style={{
+				display: 'flex',
+				flexDirection: 'column',
+				gap: 5,
+				maxWidth: 350,
+			}}
+		>
+			<h3>Название товара</h3>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis rem
+				nostrum numquam in, repellat recusandae? Officia quisquam dolores
+				molestias assumenda, alias consequuntur accusantium accusamus aliquam
+				itaque quaerat, expedita cumque neque.
+			</p>
+		</div>
+	)
+}
+
+export default Product
